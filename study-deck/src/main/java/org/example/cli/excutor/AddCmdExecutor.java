e218794c7ff435fddb67f4e6f8e95684460988e9
GGojae <woajrsla100@gmail.com>
2026-04-17 13:47:50 +0900

    chore: add 커맨드 executor 클래스 생성

---
+++ study-deck/src/main/java/org/example/cli/excutor/AddCmdExecutor.java
@@ -0,0 +1,16 @@
+package org.example.cli.excutor;
+
+import org.example.cli.model.Command;
+
+public class AddCmdExecutor implements CommandExecutor{
+
+    @Override
+    public String canResolvedCommand() {
+        return "add";
+    }
+
+    @Override
+    public void execute(Command command) {
+
+    }
+}
