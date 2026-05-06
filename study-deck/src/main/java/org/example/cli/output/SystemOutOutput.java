b0a5a4d95b768ca234e89d933d372d0469f6907a
GGojae <woajrsla100@gmail.com>
2026-05-06 12:11:08 +0900

    style(cli-output) : console message 에 컬러 도입

--- study-deck/src/main/java/org/example/cli/output/SystemOutOutput.java
+++ study-deck/src/main/java/org/example/cli/output/SystemOutOutput.java
@@ -1,6 +1,7 @@
 package org.example.cli.output;
 
-import org.example.core.category.model.CategoryCapture;
+import org.example.core.application.category.dto.response.CategoryCapture;
+import org.example.core.application.subcategory.dto.response.SubCategoryCapture;
 
 import java.util.List;
 
@@ -10,9 +11,26 @@ public class SystemOutOutput implements Output{
         categories.stream()
                 .map(c -> {
                     if (c.id().equals(focusCategoryId)) {
-                        return " * " + c.name();
+                        return ConsoleColor.GREEN + "[" + c.id() + "] " + c.name() + ConsoleColor.RESET;
                     }
-                    return c.name();
+                    return "[" + c.id() + "] " + c.name();
+                })
+                .forEach(System.out::println);
+    }
+
+    @Override
+    public void errorMessage(String message) {
+        System.out.println(ConsoleColor.RED + "[ERROR] " + message + ConsoleColor.RESET );
+    }
+
+    @Override
+    public void subAndCurrentSub(List<SubCategoryCapture> subCategories, Long focusSubCatId) {
+        subCategories.stream()
+                .map(c -> {
+                    if (c.id().equals(focusSubCatId)) {
+                        return ConsoleColor.GREEN + "[" + c.id() + "] " + c.name() + ConsoleColor.RESET;
+                    }
+                    return "[" + c.id() + "] " + c.name();
                 })
                 .forEach(System.out::println);
     }
